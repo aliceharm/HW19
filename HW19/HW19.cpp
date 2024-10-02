b469dce04391c98ea29a850235cd5f30129f4947
Alice Shu <alice@e26.ru>
2024-10-02 12:30:54 +0800

    Добавьте файлы проекта.

---
+++ HW19/HW19.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Animal {
+public:
+    virtual void Voice() {
+        cout << "Animal sound!" << endl;
+    }
+};
+
+
+class Dog : public Animal {
+public:
+    void Voice() override {
+        cout << "Woof!" << endl;
+    }
+};
+
+
+class Cat : public Animal {
+public:
+    void Voice() override {
+        cout << "Meow!" << endl;
+    }
+};
+
+
+class Mouse : public Animal {
+public:
+    void Voice() override {
+        cout << "Pee-pee!" << endl;
+    }
+};
+
+int main() {
+   
+    vector<Animal*> animals;
+
+    animals.push_back(new Dog());
+    animals.push_back(new Cat());
+    animals.push_back(new Mouse());
+
+    for (Animal* animal : animals) {
+        animal->Voice();
+    }
+
+    for (Animal* animal : animals) {
+        delete animal;
+    }
+
+    return 0;
+}
+
